eaecad5261daef29071b66569d45f3bee481368a
srinuraeni4596 <50136294+srinuraeni4596@users.noreply.github.com>
2019-05-16 11:36:14 +0700

    Create if-else.js

---
+++ if-else.js
@@ -0,0 +1,12 @@
+function jalanSatu(){
+  var nilai = 2;
+  if (nilai === 3) {
+    console.log ("benar")
+  } else if (nilai === 2) {
+    console.log("dua")
+  }else{
+    console.log("salah")
+  }
+}
+
+jalanSatu()
