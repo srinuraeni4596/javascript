5337778b32d024c321f14ecc777b28603b42b1d9
srinuraeni4596 <50136294+srinuraeni4596@users.noreply.github.com>
2019-05-16 14:01:38 +0700

    Create array-shift.js

---
+++ array-shift.js
@@ -0,0 +1,8 @@
+function panggilshift(){
+  var kota = ['jakarta','bandung','malang','makasar'];
+ console.log (kota);
+ console.log ("==============")
+  kota = kota.shift();
+  return kota
+}
+panggilshift()
