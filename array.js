1dd1cae853626d02bafea0b49f433f823506d44e
srinuraeni4596 <50136294+srinuraeni4596@users.noreply.github.com>
2019-05-16 11:37:48 +0700

    Create array.js

---
+++ array.js
@@ -0,0 +1,8 @@
+function palnggilArray (){
+  var data = [1,2,3,4,5,6]
+    var nama = ["abi","abi2","abi3"]
+console.log (data.lenght)
+console.log (nama.lenght)
+console.log (nama[2])
+}
+palnggilArray()
