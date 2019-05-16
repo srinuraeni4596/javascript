95166911f925314c9a5ee33bc6be51d8c5b93155
srinuraeni4596 <50136294+srinuraeni4596@users.noreply.github.com>
2019-05-16 11:35:37 +0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,15 @@
+function jalanSatu() {
+  console.log("jalan satu")
+}
+
+const jalanDua = function(){
+  console.log("jalan dua")
+}
+
+jalanSatu()
+jalanDua()
+
+const jalanTiga = ()=>{
+  concole.log("Jalan Tiga")
+}
+jalanTiga 
