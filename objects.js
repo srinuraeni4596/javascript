2f46d3d93a5f9aec2e26ed77662ae0ca8ca8d77d
srinuraeni4596 <50136294+srinuraeni4596@users.noreply.github.com>
2019-05-16 11:40:08 +0700

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,10 @@
+function panggilObject(){
+  var mobil = {
+    type: 'sedan',
+    harga:2000000,
+    warna: 'putih',
+  }
+  console.log (mobil)
+console.log (mobil.warna)
+  }
+panggilObject() 
