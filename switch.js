9bf0f0cd8810de99e3a332ee5b01ddf96bfa0aa7
srinuraeni4596 <50136294+srinuraeni4596@users.noreply.github.com>
2019-05-16 11:41:23 +0700

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,22 @@
+function testingSwitch () {
+  var nilai = 8;
+
+  switch(nilai){
+case 10:
+console.log ("sangat bagus");
+break;
+
+case 8:
+console.log ("baik")
+break;
+
+case 6:
+console.log ("cukup");
+break;
+
+default:
+console.log ("lainnya")
+  }
+}
+
+testingSwitch ()
